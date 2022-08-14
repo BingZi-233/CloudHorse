2db8df1b409004513216af225fe568b46ac31389
lhby233 <lhby233@outlook.com>
2022-08-14 20:51:43 +0800

    Update
    [+] 移除冗余的方法

--- src/main/kotlin/online/bingzi/cloud/horse/internal/util/DatabaseUtil.kt
+++ src/main/kotlin/online/bingzi/cloud/horse/internal/util/DatabaseUtil.kt
@@ -22,13 +22,6 @@ fun ActionSelect.userIndex(ownerData: OwnerData) {
     }
 }
 
-fun ActionUpdate.userIndex(player: Player) {
-    when (DatabaseUserIndex.INSTANCE) {
-        DatabaseUserIndex.NAME -> where { "name" eq player.name }
-        DatabaseUserIndex.UUID -> where { "uuid" eq player.uniqueId.toString() }
-    }
-}
-
 fun ActionUpdate.userIndex(ownerData: OwnerData) {
     when (DatabaseUserIndex.INSTANCE) {
         DatabaseUserIndex.NAME -> where { "name" eq ownerData.name }
@@ -40,12 +33,5 @@ fun ActionDelete.userIndex(player: Player) {
     when (DatabaseUserIndex.INSTANCE) {
         DatabaseUserIndex.NAME -> where { "name" eq player.name }
         DatabaseUserIndex.UUID -> where { "uuid" eq player.uniqueId.toString() }
-    }
-}
-
-fun ActionDelete.userIndex(ownerData: OwnerData) {
-    when (DatabaseUserIndex.INSTANCE) {
-        DatabaseUserIndex.NAME -> where { "name" eq ownerData.name }
-        DatabaseUserIndex.UUID -> where { "uuid" eq ownerData.uuid }
     }
 }
