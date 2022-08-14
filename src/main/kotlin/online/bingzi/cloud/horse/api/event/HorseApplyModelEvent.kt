fe755be67f2ed5a50e56960931fecd294bdda8f5
lhby233 <lhby233@outlook.com>
2022-08-14 21:03:56 +0800

    Update
    [+] 更新API中的马应用类型
    [+] 更新注释

--- src/main/kotlin/online/bingzi/cloud/horse/api/event/HorseApplyModelEvent.kt
+++ src/main/kotlin/online/bingzi/cloud/horse/api/event/HorseApplyModelEvent.kt
@@ -1,7 +1,18 @@
 package online.bingzi.cloud.horse.api.event
 
+import online.bingzi.cloud.horse.api.type.HorseApplyModelType
 import org.bukkit.entity.AbstractHorse
 import org.bukkit.entity.Player
 import taboolib.platform.type.BukkitProxyEvent
 
-class HorseApplyModelEvent(val player: Player, val abstractHorse: AbstractHorse, val model: String) : BukkitProxyEvent()
+/**
+ * Horse apply model event
+ * 马应用模型事件
+ *
+ * @property player 玩家
+ * @property abstractHorse 马
+ * @property model 模型
+ * @property type 模型类型
+ * @constructor Create empty Horse apply model event
+ */
+class HorseApplyModelEvent(val player: Player, val abstractHorse: AbstractHorse, val model: String, val type: HorseApplyModelType) : BukkitProxyEvent()
---
+++ src/main/kotlin/online/bingzi/cloud/horse/api/type/HorseApplyModelType.kt
@@ -0,0 +1,18 @@
+package online.bingzi.cloud.horse.api.type
+
+/**
+ * Horse apply model type
+ * 马应用模型类型
+ *
+ * @constructor Create empty Horse apply model type
+ */
+enum class HorseApplyModelType {
+    /**
+     * 上马
+     */
+    UP,
+    /**
+     * 下马
+     */
+    DOWN,
+}
