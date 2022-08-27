509239d39e3e8023aa106d6de1feb1ac2fd80706
lhby233 <lhby233@outlook.com>
2022-08-27 17:11:39 +0800

    对模型更新以及缓存进行了改进

--- src/main/kotlin/online/bingzi/cloud/horse/internal/util/Cache.kt
+++ src/main/kotlin/online/bingzi/cloud/horse/internal/util/Cache.kt
@@ -4,6 +4,7 @@ import ltd.icecold.orangeengine.api.OrangeEngineAPI
 import online.bingzi.cloud.horse.internal.entity.OwnerData
 import org.bukkit.entity.Player
 import taboolib.common.platform.Schedule
+import taboolib.common.platform.function.warning
 
 /**
  * Cache
@@ -16,7 +17,9 @@ object Cache {
      * Cache model
      * 模型缓存
      */
-    val cacheModel: MutableList<String> = mutableListOf()
+    val cacheModel: MutableList<String> by lazy {
+        OrangeEngineAPI.getModelManager().allModelData.keys.toMutableList()
+    }
 
     /**
      * Cache owner data
@@ -31,7 +34,11 @@ object Cache {
      */
     @Schedule(period = 100, async = true)
     fun onLoadCacheModel() {
-        cacheModel.clear()
-        cacheModel.addAll(OrangeEngineAPI.getModelManager().allModelData.keys)
+        try {
+            cacheModel.clear()
+            cacheModel.addAll(OrangeEngineAPI.getModelManager().allModelData.keys)
+        } catch (ignored: Throwable) {
+            warning("更新模型缓存失败, 原因: ${ignored.message}")
+        }
     }
 }
